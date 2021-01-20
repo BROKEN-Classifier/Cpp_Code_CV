4cb9678f5cfe90a0c2364aa215f91dc39a68aa35
BROKEN-Semiconductor <67321026+BROKEN-Classifier@users.noreply.github.com>
2021-01-20 14:33:36 +0100

    Repository clean up
    Removal of all Hangman files
    Commit with files from an older version

--- Hangman/main.cpp
+++ Hangman/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "mainwindow.h"
 
 #include <QApplication>
@@ -10,16 +9,3 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
-=======
-#include "mainwindow.h"
-
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
->>>>>>> 0cbd34bede97bc5f76feb813254d0115423d4e55
